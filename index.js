338c004ae2e473e1441fc3122cb78e187803988d
rowheen <rowin.quindoza@gmail.com>
2024-11-16 01:10:13 +0800

    added add post feature

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 8080;
+
+let tasks = [];
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+
+app.get("/add", (req,res) => {
+    res.render("edit.ejs");
+});
+
+app.post("/add", (req,res) => {
+    const id = tasks.length + 1;
+    const title = req.body.title;
+    const description = req.body.description;
+    const timestamp = new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" });
+    
+    const newTask = {id: id, title: title, description:description, timestamp:timestamp}
+    tasks.push(newTask);
+    console.log(tasks);
+});
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+});
